a1ae1a40a846e0ddfe36502d7050524292e5aec5
Isaac Ingles <isaac.ingles@gmail.com>
2021-04-01 12:06:38 -0600

    got basic markup and styles done for navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,18 +6,20 @@ const NavBar = props => {
     return (
         <header className="main-navbar">
             <div className="container">
-                <div className="logo">
-                    <h1>Chaotic Royale</h1>
-                </div>
-                
-                <nav className="main-nav">
-                    <a href="#">Blackjack</a>
-                    <a href="#">Other game</a>
-                </nav>
+                <div className="row align-items-center">
+                    <div className="logo mr-lg-5">
+                        <h1>Chaotic Royale</h1>
+                    </div>
+                    
+                    <nav className="main-nav">
+                        <a href="#" className="current">Blackjack</a>
+                        <a href="#">Other game</a>
+                    </nav>
 
-                <div className="score-balance">
-                    <h3 className="high-score">High Score: $150,000</h3> 
-                    <h3 className="balance">Balance: $20,000</h3>
+                    <div className="d-flex score-balance justify-self-end">
+                        <h3 className="high-score pr-lg-5 mr-lg-5">High Score: $150,000</h3>
+                        <h3 className="balance">Balance: $20,000</h3>
+                    </div>
                 </div>
             </div>
         </header>
