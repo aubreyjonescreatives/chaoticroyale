131dae75213783cfa889fc9bb5ac2f651f1a9d9b
Brayden England <bray.patriot@gmail.com>
2021-04-05 14:55:22 -0600

    CardArea component, App.js passes cards as props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.scss';
 import NavBar from './components/navbar/NavBar'
 import DealerArea from './components/DealerArea/DealerArea'
 import PlayerArea from './components/PlayerArea/PlayerArea'
+import CardArea from './components/CardArea/CardArea'
 
 
 
 function App() {
+
+  // TODO: the logic for blackjack needs to be moved into a blackjack component, since there will be different games.
+  // it looks like the GameArea component can just be changed to the blackjack component.
+  // use react router to switch between the different games / pages 
+
+  const [userCards, setUserCards] = useState([])
+  const [dealerCards, setDealerCards] = useState([])
+
+  const fetchCards = async () => {
+    // TODO: this can probably be optimized. probably don't have to make two axios requests.
+    axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`)
+    .then((res) => {
+      setDealerCards([...dealerCards, res.data.cards[0]])
+    })
+    axios.get(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`)
+    .then((res) => {
+      setUserCards([...userCards, res.data.cards[0]])
+    })
+  }
+
+  useEffect(() => {
+    fetchCards()
+  }, [])
+  
   return (
     <div className="App">
       <NavBar />
-      <DealerArea />
-      <PlayerArea />
+      <CardArea cards={dealerCards} name="Dealer" />
+      <CardArea cards={userCards} name="Player" />
     </div>
   );
 }
---
+++ src/components/CardArea/CardArea.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import './CardArea.scss'
+
+const CardArea = (props) => {
+
+  return (
+    <div className="card-area-container">
+      <div className="area-name">{props.name}</div>
+      <div className="AreaCard">
+      { props.cards.map((card, index) => {
+        console.log(card)
+        return (
+          <div className="cardItem" key={index}>
+            <img src={`./images/cards/${card.code.toLowerCase()}.svg`} alt={'Card ' + card.code} />
+          </div>
+        )
+      }) }
+      </div>
+    </div>
+  )
+}
+export default CardArea; 
