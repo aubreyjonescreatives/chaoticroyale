8edb63a6d5b2ae271a474f59a79210138c36dcb5
Brayden England <bray.patriot@gmail.com>
2021-04-07 14:50:28 -0600

    File structure for Blackjack components updated

--- src/components/Blackjack/Blackjack.js
+++ src/components/Blackjack/Blackjack.js
@@ -2,14 +2,10 @@ import { useEffect, useState } from 'react'
 import './Blackjack.scss'
 
 import axios from 'axios'
-import CardArea from '../CardArea/CardArea'
-import ActionArea from '../ActionArea/ActionArea'
+import CardArea from './CardArea/CardArea'
+import ActionArea from './ActionArea/ActionArea'
 
 const Blackjack = props => {
-
-    // TODO: the logic for blackjack needs to be moved into a blackjack component, since there will be different games.
-    // it looks like the GameArea component can just be changed to the blackjack component.
-    // use react router to switch between the different games / pages 
 
     const [userCards, setUserCards] = useState([])
     const [dealerCards, setDealerCards] = useState([])
