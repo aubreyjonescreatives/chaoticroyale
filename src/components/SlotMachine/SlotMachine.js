14d9a7756919ca6807fa9e6459a41a1a08fa1f7c
Isaac Ingles <isaac.ingles@gmail.com>
2021-04-07 12:58:55 -0600

    added some game logic to slot machine

--- src/components/SlotMachine/SlotMachine.js
+++ src/components/SlotMachine/SlotMachine.js
@@ -5,24 +5,60 @@ import Tumbler from './Tumbler/Tumbler'
 
 
 const SlotMachine = props => {
+    // Random numbers for tumblers, win state true/false, and the winning value
     const [randomNumbers, setRandomNumbers] = useState([1,1,1])
+    const [winState, setWinState] = useState(false)
+    const [winValue, setWinValue] = useState(0)
     
     const generateNumbers = () => {
-        setRandomNumbers([randomNumber(), randomNumber(), randomNumber()])
+        // Reset the win value to 0
+        setWinValue(0)
+
+        // Generate a random number for each element in the randomNumbers array
+        const tumblerValues = [randomNumber(), randomNumber(), randomNumber()]       
+        setRandomNumbers(tumblerValues)
     }
+
+    const checkWin = n => {
+        // Check for win state -- compare the values of the tumblers
+        if ( n[0] === n[1] && n[0] === n[2] ) return true
+        
+        return false
+    } 
+   
+
+    useEffect(()=> {
+
+        // When the random numbers update, set the win state
+        setWinState( checkWin(randomNumbers) )
+
+        // If winState is true, Set the winValue to whatever the first number is (they should all be equal)
+        if(winState) setWinValue(randomNumbers[0])
+
+    }, [winState, randomNumbers])
+
 
     const randomNumber = () => {
         // Generate a random number between 1 and 8
-        return Math.floor(Math.random() * 8) + 1
+        return Math.floor(Math.random() * 7) + 1
     }
 
     return (
-        <div className="SlotMachine">
-            <Tumbler number={ randomNumbers[0] }/>
-            <Tumbler number={ randomNumbers[1] }/>
-            <Tumbler number={ randomNumbers[2] }/>
+        <div className="container slot-machine">
+            <div className="slot-status">
+                <h1>{ winState ? 'Win!' : 'Nope.' }</h1>
+                <h2>{ winState ? `You won ${winValue}!` : 'You win nothing.' }</h2>
+            </div>
 
-            <button onClick={ generateNumbers }>Go</button>
+            <div className="row slot-tumblers">
+                <Tumbler number={ randomNumbers[0] }/>
+                <Tumbler number={ randomNumbers[1] }/>
+                <Tumbler number={ randomNumbers[2] }/>
+            </div>            
+
+            <div className="row slot-actions">
+                <button className="slot-btn" onClick={ generateNumbers }>Pull</button>
+            </div>            
         </div>      
 
     )
