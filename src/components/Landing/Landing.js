09b422d976c6b6af4031292647e78980267dc80e
Brayden England <bray.patriot@gmail.com>
2021-04-07 14:23:01 -0600

    Landing page component started

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Landing.scss'
+
+const Landing = () => {
+  
+  return (
+    <div className="Landing ">
+      Landing page. Click on a game in the nav bar to load that game. 
+    </div>
+  );
+}
+
+export default Landing
